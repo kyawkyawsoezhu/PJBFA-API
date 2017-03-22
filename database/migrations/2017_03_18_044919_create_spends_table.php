8d6802e3ce6101159b28d57d6601da3bce1344a2
Kyaw Kyaw Soe <kyawkyaw@global-connect.asia>
2017-03-22 16:37:41 +0630

    :card_file_box: created migrations and models

---
+++ database/migrations/2017_03_18_044919_create_spends_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spends', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('amount');
+            $table->string('currency');
+            $table->text('note')->nullable();
+            $table->date('spend_date');
+            $table->enum('spend_for_type',['projects','offices']);
+            $table->integer('spend_for_id');
+            $table->integer('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spends');
+    }
+}
